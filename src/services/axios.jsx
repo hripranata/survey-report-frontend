742e3a53f9fd79773df1438fcb24c498efa3c1ba
Hari Pranata <hripranata@gmail.com>
2023-10-30 20:18:14 +0700

    change base url

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 export default axios.create({
     withCredentials: true,
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: 'https://survey-report-api.onrender.com'
 });
